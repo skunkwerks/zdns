ceee71a892aa18ca571226aabc76e90ba8c4f13d
xackus <14938807+xackus@users.noreply.github.com>
2020-12-19 09:13:00 +0100

    expose conversion functions

--- build.zig
+++ build.zig
@@ -36,7 +36,7 @@ pub fn build(b: *bld.Builder) !void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zdns", "src/main.zig");
+    const exe = b.addExecutable("zone2json", "src/main.zig");
 
     addDependencies(exe);
 
---
+++ src/zdns.zig
@@ -0,0 +1,2 @@
+pub const ldns = @import("ldns.zig");
+pub const zone2json = @import("zone2json.zig");
